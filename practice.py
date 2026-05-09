e643d40da69aee3402dd741c0491f6b56c629c88
aloshvp <aloshvp12@gmail.com>
2026-05-10 00:02:42 +0530

    Update practice.py

--- practice.py
+++ practice.py
@@ -8,6 +8,7 @@ print(type(Age))
 print(type(Salary))
 print(type(Is_student))
 
+
 #swapping 2 numbers
 num1=5
 num2=10
@@ -18,6 +19,7 @@ num1=num2
 num2=temp
 print("before swapping",num1,num2)
 
+
 #Convert:
 #string → int
 str_val = '123'
@@ -25,11 +27,13 @@ print("before conversion",type(str_val))
 str_int = int(str_val)
 print("after conversion",type(str_int))
 
+
 #int → float
 int_val = 123
 print("before conversion",type(int_val))
 int_float_val = float(int_val)
 print("after conversion",type(int_float_val))
+
 
 #float → string
 float_val= 23.22
@@ -48,3 +52,31 @@ print(sample_tuple)
 print(sample_set)
 print(sample_dic)
 
+
+#Convert "500" into integer.
+num_str = "500"
+int_str = int(num_str)
+print(int_str,type(int_str))
+
+
+#Convert 25 into float.
+num = 25
+num_float = float(num)
+print(num_float,type(num_float))
+
+
+#Convert 99.99 into integer.
+float_num = 99.99
+float_to_int = int(float_num)
+print(float_to_int,type(float_to_int))
+
+
+#Take age input from user and convert to integer.
+age = int(input("Enter age"))
+print("age:",age)
+
+
+#Add two numbers entered by user.
+num1 = int(input("Enter first number"))
+num2 = int(input("Enter second number"))
+print("sum is:",num1 + num2)
