276b72259ce0022873ef45dc125ad6a918042e7b
aloshvp <aloshvp12@gmail.com>
2026-05-10 10:48:21 +0530

    Update list_practice.py

--- list_practice.py
+++ list_practice.py
@@ -2,3 +2,39 @@
 items = ['a','b',1,'c','3']
 print(items[1:-1])      
 
+
+#Reverse list using slicing.
+items = ['a','b',1,'c','3']
+print(items[::-1])  
+#items.reverse()
+#print(items)
+#print(list(reversed(items)))
+
+#Insert value at specific position.
+items = ['a','b',1,'c','3']
+items.insert(0,"x")
+print(items)
+
+#Update second element.
+items = ['a','b',1,'c','3']
+items[1]='x'
+print(items)
+
+
+#Delete element using: del
+items = ['a','b',1,'c','3','b']
+del items[0]
+print(items)
+
+
+# Sum only integers
+items = ['a', 'b', 1, 'c', '3', 'b']
+total = sum(i for i in items if isinstance(i, int))
+print(total)
+
+
+#Find maximum number in list.
+items = ['a','b',1,'c','3','b']
+print(max(i for i in items if isinstance(i,int)))
+items2 = [1,2,5,8,0]
+print(max(items2))
