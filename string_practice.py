0da066fdd936fa6f698fc56c9fb7ab90a437f722
aloshvp <aloshvp12@gmail.com>
2026-05-10 11:27:12 +0530

    Create string_practice.py

---
+++ string_practice.py
@@ -0,0 +1,10 @@
+#Replace unwanted characters.
+text = "Hello, World!"
+print(text.replace(",","").replace("!",""))
+
+
+#Count spaces in sentence.
+text = " Hello, World! "
+print(text.count(" "))
+
+
