95f1937885a32ac4f9b0a5a5dab33f44c114028f
aloshvp <aloshvp12@gmail.com>
2026-05-10 11:41:46 +0530

    Update string_practice.py

--- string_practice.py
+++ string_practice.py
@@ -8,3 +8,26 @@ text = " Hello, World! "
 print(text.count(" "))
 
 
+#Reverse string.
+text = "Hello, World!"
+print(text[::-1])
+
+#Print alternate characters.
+text = "Hello, World!"
+print(text[::2])
+
+
+#Count vowels in string.
+text = "Hello, World!"
+count = sum(1 for ch in text if ch in 'aeiouAEIOU')
+print(count)
+
+#Check palindrome.
+text = input("Enter a text")
+text_reverse = text[::-1]
+if text == text_reverse:
+    print(f"{text} is palindrome")
+else:
+    print(f"{text} is not palindrome")
+
+    
