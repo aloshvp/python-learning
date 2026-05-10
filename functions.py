dab65726e80aaf67f9c58373c506c0dbde1728a9
aloshvp <aloshvp12@gmail.com>
2026-05-10 15:48:19 +0530

    Update functions.py

---
+++ functions.py
@@ -0,0 +1,8 @@
+#Function to calculate the mean of a list of numbers
+def numbers_mean(numbers):       
+    print(len(numbers))
+    return sum(numbers) / len(numbers)
+
+mean_val = numbers_mean([10, 20, 30])
+print(mean_val)
+
