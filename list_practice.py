136a7def68920189930c24fcd675f9ec8e1d0594
aloshvp <aloshvp12@gmail.com>
2026-05-10 11:27:07 +0530

    Update list_practice.py

--- list_practice.py
+++ list_practice.py
@@ -38,3 +38,23 @@ items = ['a','b',1,'c','3','b']
 print(max(i for i in items if isinstance(i,int)))
 items2 = [1,2,5,8,0]
 print(max(items2))
+
+#Extract even numbers.(List Comprehension)
+items=[1,3,9,5,6,2]
+item_even=[
+    i
+    for i in items
+    if i%2==0
+]
+print(item_even)
+
+
+#Convert all names to uppercase.(List Comprehension)
+items=['abc','xyz']
+item_upper=[
+    i.upper()
+    for i in items
+]
+print(item_upper)
+
+
