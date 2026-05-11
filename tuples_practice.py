7578e03013d28469bd6e75d518d3472f9c50553d
aloshvp <aloshvp12@gmail.com>
2026-05-11 08:09:24 +0530

    Update tuples_practice.py

---
+++ tuples_practice.py
@@ -0,0 +1,14 @@
+#Create tuple with student data.
+student = ("John",18,"Data Engg","student")
+print(type(student))
+
+
+#Access tuple elements.
+student = ("John",18,"Data Engg","student")
+print(f"Name: {student[0]}")
+print(f"Age: {student[1]}")
+print(f"Course: {student[2]}")
+print(f"isStudent: {student[3]}")
+
+
+
