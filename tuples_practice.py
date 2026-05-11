e37644713eaa10ed520550a117912bdae68d4f61
aloshvp <aloshvp12@gmail.com>
2026-05-11 08:21:02 +0530

    Update tuples_practice.py

--- tuples_practice.py
+++ tuples_practice.py
@@ -11,4 +11,19 @@ print(f"Course: {student[2]}")
 print(f"isStudent: {student[3]}")
 
 
+#Convert tuple to list.
+student = ("John",18,"Data Engg","student")
+student_list = [
+    stud
+    for stud in student
+]
+student_lst = list(student)
+print(student_list)
+print(student_lst)
+
+#Remove duplicates from tuple.
+items = (1,2,'a','b',1,'c','a')
+items_unique = tuple(set(items))
+print(items_unique)
+
 
