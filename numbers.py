8d1341fd59970fb5dba1a8dacd0682e3e5f2ad21
aloshvp <aloshvp12@gmail.com>
2026-05-14 08:15:00 +0530

    Create numbers.py

---
+++ numbers.py
@@ -0,0 +1,28 @@
+#types
+x = 5
+y = 5.7
+z = 2+3j
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+x="24"
+print(type(x))
+x=int(x)
+print(type(x))
+print(x*3)
+
+x=3.14
+print(int(x))
+
+x=3
+print(float(x))
+
+x="3"
+print(float(x))
+
+
+x = 3 #real
+y = 4 #imaginary
+print(complex(x,y))
